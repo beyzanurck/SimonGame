5a897aadf5eaf60e3b01884ff34572765ee6d876
Beyzanur Kılınç <nurbeyzakilinc@gmail.com>
2021-09-07 21:11:35 -0500

    buttons created

--- Program.cs
+++ Program.cs
@@ -30,7 +30,57 @@ namespace simon
 
             // blue part
             r = 17;
-            for (int i = 0; i < 80; i+=10)
+
+            for (int i = 5; i < 10; i += 1)
+            {
+                pointX = 2 * r * Math.Cos(i * pi / 180);
+                pointY = r * Math.Sin(i * pi / 180);
+
+                int x = Convert.ToInt32(pointX);
+                int y = Convert.ToInt32(pointY);
+                Console.SetCursorPosition(20 + 60 + x, 30 + y);
+                Console.Write(" ");
+            }           
+
+            for (int i = 15; i < 90; i+=10)
+            {
+                pointX = 2 * r * Math.Cos(i * pi / 180);
+                pointY = r * Math.Sin(i * pi / 180);
+
+                int x = Convert.ToInt32(pointX);
+                int y = Convert.ToInt32(pointY);
+                Console.SetCursorPosition(20 + 60 + x, 30 + y);
+                Console.Write(".");
+
+                if (i == 15 || i == 85)
+                {
+                    for (int radius = 17; radius > 9; radius-=4)
+                    {
+                        pointX = 2 * radius * Math.Cos(i * pi / 180);
+                        pointY = radius * Math.Sin(i * pi / 180);
+
+                        x = Convert.ToInt32(pointX);
+                        y = Convert.ToInt32(pointY);
+                        Console.SetCursorPosition(20 + 60 + x, 30 + y);
+                        Console.Write(".");
+                    }
+                }             
+
+            }
+
+            for (int i = 90; i < 95; i += 1)
+            {
+                pointX = 2 * r * Math.Cos(i * pi / 180);
+                pointY = r * Math.Sin(i * pi / 180);
+
+                int x = Convert.ToInt32(pointX);
+                int y = Convert.ToInt32(pointY);
+                Console.SetCursorPosition(20 + 60 + x, 30 + y);
+                Console.Write(" ");
+            }
+
+            r = 9;
+            for (int i = 15; i < 90; i += 15)
             {
                 pointX = 2 * r * Math.Cos(i * pi / 180);
                 pointY = r * Math.Sin(i * pi / 180);
@@ -43,14 +93,184 @@ namespace simon
 
             // yellow part
             r = 17;
-            for (int i = 0; i < 80; i += 10)
+            for (int i = 95; i < 100; i += 1)
             {
                 pointX = 2 * r * Math.Cos(i * pi / 180);
                 pointY = r * Math.Sin(i * pi / 180);
 
                 int x = Convert.ToInt32(pointX);
                 int y = Convert.ToInt32(pointY);
-                Console.SetCursorPosition(20 + 60 - x, 30 + y);
+                Console.SetCursorPosition(20 + 60 + x, 30 + y);
+                Console.Write(" ");
+            }
+
+            for (int i = 100; i < 180; i += 10)
+            {
+                pointX = 2 * r * Math.Cos(i * pi / 180);
+                pointY = r * Math.Sin(i * pi / 180);
+
+                int x = Convert.ToInt32(pointX);
+                int y = Convert.ToInt32(pointY);
+                Console.SetCursorPosition(20 + 60 + x, 30 + y);
+                Console.Write(".");
+
+                if (i == 100 || i == 170)
+                {
+                    for (int radius = 17; radius > 9; radius -= 4)
+                    {
+                        pointX = 2 * radius * Math.Cos(i * pi / 180);
+                        pointY = radius * Math.Sin(i * pi / 180);
+
+                        x = Convert.ToInt32(pointX);
+                        y = Convert.ToInt32(pointY);
+                        Console.SetCursorPosition(20 + 60 + x, 30 + y);
+                        Console.Write(".");
+                    }
+                }
+            }
+
+            for (int i = 180; i < 185; i += 1)
+            {
+                pointX = 2 * r * Math.Cos(i * pi / 180);
+                pointY = r * Math.Sin(i * pi / 180);
+
+                int x = Convert.ToInt32(pointX);
+                int y = Convert.ToInt32(pointY);
+                Console.SetCursorPosition(20 + 60 + x, 30 + y);
+                Console.Write(" ");
+            }
+
+            r = 9;
+            for (int i = 100; i < 180; i += 15)
+            {
+                pointX = 2 * r * Math.Cos(i * pi / 180);
+                pointY = r * Math.Sin(i * pi / 180);
+
+                int x = Convert.ToInt32(pointX);
+                int y = Convert.ToInt32(pointY);
+                Console.SetCursorPosition(20 + 60 + x, 30 + y);
+                Console.Write(".");
+            }
+
+            // green part
+            r = 17;
+            for (int i = 185; i < 190; i += 1)
+            {
+                pointX = 2 * r * Math.Cos(i * pi / 180);
+                pointY = r * Math.Sin(i * pi / 180);
+
+                int x = Convert.ToInt32(pointX);
+                int y = Convert.ToInt32(pointY);
+                Console.SetCursorPosition(20 + 60 + x, 30 + y);
+                Console.Write(" ");
+            }
+
+            for (int i = 190; i < 270; i += 10)
+            {
+                pointX = 2 * r * Math.Cos(i * pi / 180);
+                pointY = r * Math.Sin(i * pi / 180);
+
+                int x = Convert.ToInt32(pointX);
+                int y = Convert.ToInt32(pointY);
+                Console.SetCursorPosition(20 + 60 + x, 30 + y);
+                Console.Write(".");
+
+                if (i == 190 || i == 260)
+                {
+                    for (int radius = 17; radius > 9; radius -= 4)
+                    {
+                        pointX = 2 * radius * Math.Cos(i * pi / 180);
+                        pointY = radius * Math.Sin(i * pi / 180);
+
+                        x = Convert.ToInt32(pointX);
+                        y = Convert.ToInt32(pointY);
+                        Console.SetCursorPosition(20 + 60 + x, 30 + y);
+                        Console.Write(".");
+                    }
+                }
+            }
+
+            for (int i = 270; i < 275; i += 1)
+            {
+                pointX = 2 * r * Math.Cos(i * pi / 180);
+                pointY = r * Math.Sin(i * pi / 180);
+
+                int x = Convert.ToInt32(pointX);
+                int y = Convert.ToInt32(pointY);
+                Console.SetCursorPosition(20 + 60 + x, 30 + y);
+                Console.Write(" ");
+            }
+
+            r = 9;
+            for (int i = 190; i < 270; i += 15)
+            {
+                pointX = 2 * r * Math.Cos(i * pi / 180);
+                pointY = r * Math.Sin(i * pi / 180);
+
+                int x = Convert.ToInt32(pointX);
+                int y = Convert.ToInt32(pointY);
+                Console.SetCursorPosition(20 + 60 + x, 30 + y);
+                Console.Write(".");
+            }
+
+            // red part
+            r = 17;
+            for (int i = 275; i < 280; i += 1)
+            {
+                pointX = 2 * r * Math.Cos(i * pi / 180);
+                pointY = r * Math.Sin(i * pi / 180);
+
+                int x = Convert.ToInt32(pointX);
+                int y = Convert.ToInt32(pointY);
+                Console.SetCursorPosition(20 + 60 + x, 30 + y);
+                Console.Write(" ");
+            }
+
+            for (int i = 280; i < 360; i += 10)
+            {
+                pointX = 2 * r * Math.Cos(i * pi / 180);
+                pointY = r * Math.Sin(i * pi / 180);
+
+                int x = Convert.ToInt32(pointX);
+                int y = Convert.ToInt32(pointY);
+                Console.SetCursorPosition(20 + 60 + x, 30 + y);
+                Console.Write(".");
+
+                if (i == 280 || i == 350)
+                {
+                    for (int radius = 17; radius > 9; radius -= 4)
+                    {
+                        pointX = 2 * radius * Math.Cos(i * pi / 180);
+                        pointY = radius * Math.Sin(i * pi / 180);
+
+                        x = Convert.ToInt32(pointX);
+                        y = Convert.ToInt32(pointY);
+                        Console.SetCursorPosition(20 + 60 + x, 30 + y);
+                        Console.Write(".");
+                    }
+                }
+            }
+
+            for (int i = 360; i < 365; i += 1)
+            {
+                pointX = 2 * r * Math.Cos(i * pi / 180);
+                pointY = r * Math.Sin(i * pi / 180);
+
+                int x = Convert.ToInt32(pointX);
+                int y = Convert.ToInt32(pointY);
+                Console.SetCursorPosition(20 + 60 + x, 30 + y);
+                Console.Write(" ");
+            }
+
+            r = 9;
+            for (int i = 280; i < 360; i += 15)
+            {
+                pointX = 2 * r * Math.Cos(i * pi / 180);
+                pointY = r * Math.Sin(i * pi / 180);
+
+                int x = Convert.ToInt32(pointX);
+                int y = Convert.ToInt32(pointY);
+                Console.SetCursorPosition(20 + 60 + x, 30 + y);
                 Console.Write(".");
             }
 
